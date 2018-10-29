008cdeaca9c7904e8a6cff77180cf2db5ef9bc1b
Innnovacis SRL <innnovacissrl@MacBook-Pro-de-Innnovacis.local>
2018-10-29 15:36:22 -0500

    IOS

---
+++ AntapaccayQR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AntapaccayQR
+//
+//  Created by Innnovacis SRL on 29/10/18.
+//  Copyright © 2018 Innnovacis SRL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AntapaccayQRTests/AntapaccayQRTests.swift
@@ -0,0 +1,36 @@
+//
+//  AntapaccayQRTests.swift
+//  AntapaccayQRTests
+//
+//  Created by Innnovacis SRL on 29/10/18.
+//  Copyright © 2018 Innnovacis SRL. All rights reserved.
+//
+
+import XCTest
+@testable import AntapaccayQR
+
+class AntapaccayQRTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
