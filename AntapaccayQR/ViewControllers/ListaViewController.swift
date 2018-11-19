00e66fc6ffd439579582562466a7a87f00f6aa35
Innnovacis SRL <innnovacissrl@MacBook-Pro-de-Innnovacis.local>
2018-11-19 17:43:10 -0500

    Agregado de nueva patalla de manejo de items

---
+++ AntapaccayQR/ViewControllers/ListaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListaViewController.swift
+//  AntapaccayQR
+//
+//  Created by Innnovacis SRL on 11/19/18.
+//  Copyright © 2018 Innnovacis SRL. All rights reserved.
+//
+
+import UIKit
+
+class ListaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
