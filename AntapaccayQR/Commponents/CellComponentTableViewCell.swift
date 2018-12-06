659bb2db5caae3287497ea0c16500b6ebd1898e9
Yhonatan Jesus <yiquiapaza@gmail.com>
2018-12-06 11:32:17 -0500

    Nuevo vistas y servicios

---
+++ AntapaccayQR/Commponents/CellComponentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CellComponentTableViewCell.swift
+//  AntapaccayQR
+//
+//  Created by Innnovacis SRL on 12/5/18.
+//  Copyright © 2018 Innnovacis SRL. All rights reserved.
+//
+
+import UIKit
+
+class CellComponentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ AntapaccayQR/Models/Reporte.swift
@@ -0,0 +1,9 @@
+//
+//  Reporte.swift
+//  AntapaccayQR
+//
+//  Created by Innnovacis SRL on 12/5/18.
+//  Copyright © 2018 Innnovacis SRL. All rights reserved.
+//
+
+import Foundation
